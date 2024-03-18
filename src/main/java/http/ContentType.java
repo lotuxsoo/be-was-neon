c90ba614ba514127558f6570efec868ec9aa7645
lotuxsoo <tndus5383@gmail.com>
2024-03-18 18:07:01 +0900

    fix: ContentType 한줄로 보이도록 수정

--- src/main/java/http/ContentType.java
+++ src/main/java/http/ContentType.java
@@ -1,10 +1,13 @@
 package http;
 
 public enum ContentType {
-    HTML("html", "text/html;charset=utf-8"), CSS("css", "text/css;charset=utf-8"), JS("js",
-            "application/javascript"), ICO("ico",
-            "image/x-icon"), PNG("png", "image/png"),
-    JPG("jpeg", "image/jpeg"), SVG("svg", "image/svg+xml");
+    HTML("html", "text/html;charset=utf-8"),
+    CSS("css", "text/css;charset=utf-8"),
+    JS("js", "application/javascript"),
+    ICO("ico", "image/x-icon"),
+    PNG("png", "image/png"),
+    JPG("jpeg", "image/jpeg"),
+    SVG("svg", "image/svg+xml");
 
     private String name;
     private String contentType;
