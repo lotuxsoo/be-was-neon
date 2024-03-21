d4cc2055549129da4a91b39a06640defda00bf20
lotuxsoo <tndus5383@gmail.com>
2024-03-21 11:26:59 +0900

    fix: HttpRequest를 key,value 형태로 Map에 저장함

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -6,7 +6,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +17,14 @@ public class HttpRequest {
     private final String startLine;
     private final String method;
     private final String resource;
+    private Map<String, String> headers;
+    private String body;
 
     public HttpRequest(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         List<String> requests = new ArrayList<>();
+        headers = new HashMap<String, String>();
+
         String line;
         while ((line = br.readLine()) != null && !line.isEmpty()) {
             requests.add(line);
@@ -27,10 +33,15 @@ public class HttpRequest {
         startLine = requests.get(0);
         method = startLine.split(" ")[0];
         resource = startLine.split(" ")[1];
-        for (String request : requests) {
-            logger.debug(request);
+
+        for (int i = 1; i < requests.size(); i++) {
+            String[] split = requests.get(i).split(": ");
+            headers.put(split[0], split[1]);
         }
 
+        for (String key : headers.keySet()) {
+            logger.debug(key + " " + headers.get(key));
+        }
     }
 
     public String getMethod() {
