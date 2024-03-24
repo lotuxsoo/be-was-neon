4803120916c287b14d7d2d6d2de891e497b65925
lotuxsoo <tndus5383@gmail.com>
2024-03-24 14:24:13 +0900

    feat: url과 handler 구현체를 mapping하는 map을 가지는 RequestHandlerMapper 생성

---
+++ src/main/java/webserver/handler/CreateHandler.java
@@ -0,0 +1,11 @@
+package webserver.handler;
+
+import http.HttpRequest;
+import http.HttpResponse;
+
+public class CreateHandler implements RequestHandler{
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) {
+
+    }
+}
