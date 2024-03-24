b7bdc4dde3084fcb4e8358c48eedd25bc1e5a352
lotuxsoo <tndus5383@gmail.com>
2024-03-25 00:44:54 +0900

    fix: HttpResponse 메서드 수정

--- src/main/java/webserver/MainHandler.java
+++ src/main/java/webserver/MainHandler.java
@@ -12,10 +12,13 @@ import java.io.OutputStream;
 import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import webserver.handler.CreateHandler;
+import webserver.handler.HomeHandler;
+import webserver.handler.LoginHandler;
+import webserver.handler.RequestHandler;
 
 public class MainHandler implements Runnable {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private static final String DEFAULT_PATH = "./src/main/resources/static/";
     private final Socket connection;
 
     public MainHandler(Socket connectionSocket) {
@@ -29,49 +32,26 @@ public class MainHandler implements Runnable {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             HttpRequest httpRequest = new HttpRequest(in);
-
             String method = httpRequest.getMethod();
             String resource = httpRequest.getResource();
-
-            String contentType = "text/html";
-
-            // 클라이언트에게 응답을 전송
-            DataOutputStream dos = new DataOutputStream(out);
             HttpResponse httpResponse = new HttpResponse();
 
-            if (method.equals("GET")) {
-                // 파일 내용을 읽어들임
-                File file = new File(DEFAULT_PATH + resource);
-                if (file.exists() && !file.isDirectory()) {
-                    String ext = resource.split("\\.")[1];
-                    for (ContentType type : ContentType.values()) {
-                        if (type.getName().equals(ext)) {
-                            contentType = type.getContentType();
-                        }
-                    }
-                    byte[] body = readFileContent(file);
-                    httpResponse.response200Header(dos, body, contentType);
-                } else {
-                    httpResponse.response404Header(dos);
-                }
+            RequestHandlerMapper handlerMapper = new RequestHandlerMapper();
+
+            RequestHandler handler = handlerMapper.getHandler(resource);
+            handler.handle(httpRequest, httpResponse);
+
+            if (method.equals("GET") && resource.endsWith(".html")) {
+                // HomeHandler
             } else if (method.equals("POST") && resource.startsWith("/create")) {
-                httpResponse.response302Header(dos);
+                // CreateHandler
+                // httpResponse.response302Header(dos);
             } else if (method.equals("POST") && resource.startsWith("/login")) {
-                String sessionId = "";
-                httpResponse.response302HeaderWithCookie(dos, sessionId);
+                // LoginHandler
+                // httpResponse.response302HeaderWithCookie(dos, sessionId);
             }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
-    }
-
-    private byte[] readFileContent(File file) {
-        byte[] buffer = new byte[(int) file.length()];
-        try (FileInputStream fis = new FileInputStream(file)) {
-            fis.read(buffer);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return buffer;
     }
 }
--- src/main/java/webserver/handler/HomeHandler.java
+++ src/main/java/webserver/handler/HomeHandler.java
@@ -1,12 +1,43 @@
 package webserver.handler;
 
+import http.ContentType;
 import http.HttpRequest;
 import http.HttpResponse;
-import java.util.logging.Handler;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class HomeHandler implements RequestHandler {
+    private static final String DEFAULT_PATH = "./src/main/resources/static/";
+
     @Override
     public void handle(HttpRequest request, HttpResponse response) {
+        String resource = request.getResource();
+        File file = new File(DEFAULT_PATH + resource);
 
+        String contentType = "text/html";
+
+        if (file.exists() && !file.isDirectory()) {
+            String ext = resource.split("\\.")[1];
+            for (ContentType type : ContentType.values()) {
+                if (type.getName().equals(ext)) {
+                    contentType = type.getContentType();
+                }
+            }
+            byte[] body = readFileContent(file);
+            response.response200Header(dos, body, contentType);
+        } else {
+            response.response404Header(dos);
+        }
+    }
+
+    private byte[] readFileContent(File file) {
+        byte[] buffer = new byte[(int) file.length()];
+        try (FileInputStream fis = new FileInputStream(file)) {
+            fis.read(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return buffer;
     }
 }
