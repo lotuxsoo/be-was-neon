bcdfebbc0cd54b7d1647c83a7bfa36e8f78c464b
lotuxsoo <tndus5383@gmail.com>
2024-03-26 15:59:39 +0900

    chore: 안쓰는 주석 삭제

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -64,40 +64,4 @@ public class HttpResponse {
             logger.error(e.getMessage());
         }
     }
-
-    /*
-    public void response200Header(DataOutputStream dos, byte[] body,
-                                  String contentType) throws IOException {
-        dos.writeBytes("HTTP/1.1 200 OK\r\n");
-        dos.writeBytes("Content-Type: " + contentType + "\r\n");
-        dos.writeBytes("Content-Length: " + body.length + "\r\n");
-        dos.writeBytes("\r\n");
-        dos.write(body, 0, body.length);
-        dos.flush();
-    }
-
-    public void response302Header(DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found\r\n");
-        dos.writeBytes("Location: /index.html");
-        dos.writeBytes("\r\n");
-        dos.flush();
-    }
-
-    public void response302HeaderWithCookie(DataOutputStream dos, String sessionId) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found\r\n");
-        dos.writeBytes("Location: /index.html");
-        dos.writeBytes("Set-Cookie: sid=" + sessionId + ";" + "Path=/");
-        dos.writeBytes("\r\n");
-        dos.flush();
-    }
-
-    public void response404Header(DataOutputStream dos) throws IOException {
-        String message = "404 Not Found";
-        dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
-        dos.writeBytes("Content-Type: text/plain\r\n");
-        dos.writeBytes("Content-Length: " + message.length() + "\r\n");
-        dos.writeBytes(message);
-        dos.flush();
-    }
-    */
 }
