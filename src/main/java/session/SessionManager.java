1383decbbaa06c5c905f2a9fbc2aa2c8fcb646c8
lotuxsoo <tndus5383@gmail.com>
2024-03-26 15:04:14 +0900

    feat: 세션ID와 유저ID를 매핑하는 정보를 가진 SessionManager 작성

--- src/main/java/session/SessionManager.java
+++ src/main/java/session/SessionManager.java
@@ -1,11 +1,24 @@
 package session;
 
+import db.Database;
 import java.util.HashMap;
 import java.util.Map;
 import model.User;
 
 public class SessionManager {
-    private static final Map<String, User> loginUsers = new HashMap<>();
+    private static final Map<String, String> sessions = new HashMap<>();
 
+    public static void addSession(String sid, String userId) {
+        sessions.put(sid, userId);
+    }
+
+    public static void removeSession(String sid) {
+        sessions.remove(sid);
+    }
+
+    public static User findUserBySessionId(String sid) {
+        String userId = sessions.get(sid);
+        return Database.findUserById(userId);
+    }
 
 }
