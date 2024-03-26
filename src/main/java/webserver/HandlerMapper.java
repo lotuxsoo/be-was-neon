c563e74b292c354b4bc2bfc55e1283651468cf03
lotuxsoo <tndus5383@gmail.com>
2024-03-26 15:53:33 +0900

    chore: 안쓰는 파일 삭제

--- src/main/java/webserver/HandlerMapper.java
+++ src/main/java/webserver/HandlerMapper.java
@@ -4,14 +4,12 @@ import http.HttpRequest;
 import java.util.HashMap;
 import java.util.Map;
 import webserver.handler.CreateHandler;
-import webserver.handler.HomeHandler;
 import webserver.handler.LoginHandler;
 import webserver.handler.RequestHandler;
 import webserver.handler.StaticFileHandler;
 
 public class HandlerMapper {
     private final Map<String, RequestHandler> map = new HashMap<>() {{
-        put("/", new HomeHandler());
         put("/user/create", new CreateHandler());
         put("/user/login", new LoginHandler());
     }};
