561fbb36823e665e2bd7ea144929ffd4f2c74075
lotuxsoo <tndus5383@gmail.com>
2024-03-26 14:16:57 +0900

    feat: LoginHandler 분기문에 따른 리다이렉트

--- src/main/java/webserver/handler/LoginHandler.java
+++ src/main/java/webserver/handler/LoginHandler.java
@@ -16,12 +16,15 @@ public class LoginHandler implements RequestHandler {
     @Override
     public void handle(HttpRequest httpRequest, HttpResponse httpResponse) {
         Map<String, String> paramMap = httpRequest.getParamMap();
-
         String userId = paramMap.get("userId");
         String password = paramMap.get("password");
         User user = Database.findUserById(userId);
 
-        if (user != null && user.getUserId().equals(userId) && user.getPassword().equals(password)) {
+        if (user == null) {
+            httpResponse.addStatus(HttpStatus.FOUND);
+            httpResponse.addLocation("/index.html");
+            logger.debug("해당하는 User를 찾지 못했습니다.");
+        } else if (user.getUserId().equals(userId) && user.getPassword().equals(password)) {
             String cookie = String.format("sid=%s;", UUID.randomUUID());
             httpResponse.addStatus(HttpStatus.FOUND);
             httpResponse.addCookie(cookie);
@@ -30,6 +33,7 @@ public class LoginHandler implements RequestHandler {
         } else {
             httpResponse.addStatus(HttpStatus.NOT_FOUND);
             httpResponse.addLocation("/login/failed.html");
+            logger.debug("로그인에 실패했습니다.");
         }
     }
 }
