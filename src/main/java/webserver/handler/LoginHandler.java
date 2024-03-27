8d0297bf27faaf1191d975a7f58cc3d570709a10
lotuxsoo <tndus5383@gmail.com>
2024-03-27 11:38:14 +0900

    feat: 세션db에 세션 추가

--- src/main/java/webserver/handler/LoginHandler.java
+++ src/main/java/webserver/handler/LoginHandler.java
@@ -1,6 +1,7 @@
 package webserver.handler;
 
 import db.Database;
+import db.SessionStore;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.http.HttpStatus;
@@ -22,17 +23,18 @@ public class LoginHandler implements RequestHandler {
         User user = Database.findUserById(userId); // db에서 유저 찾아옴
 
         if (user == null) {
-            //  httpResponse.setStatus(HttpStatus.FOUND);
+            httpResponse.setStatus(HttpStatus.FOUND);
             httpResponse.setLocation("/index.html");
             logger.debug("[로그인] 해당하는 유저가 db에 없습니다.");
-        } else if (user.getUserId().equals(userId) && user.getPassword().equals(password)) {
-            String cookie = String.format("sid=%s;", UUID.randomUUID());
+        } else if (user.getUserId().equals(userId) && user.getPassword().equals(password)) { // 정보 일치
+            String sid = UUID.randomUUID().toString();
             httpResponse.setStatus(HttpStatus.FOUND);
-            httpResponse.setCookie(cookie);
+            httpResponse.setCookie(String.format("sid=%s;", sid));
             httpResponse.setLocation("/main/index.html");
+            SessionStore.addSession(sid, user.getUserId()); // 세션db에 세션 추가
             logger.debug("[로그인] 로그인이 성공했습니다. {}", user);
         } else {
-            //     httpResponse.setStatus(HttpStatus.FOUND);
+            httpResponse.setStatus(HttpStatus.FOUND);
             httpResponse.setLocation("/login/failed.html");
             logger.debug("[로그인] 로그인에 실패했습니다.");
         }
